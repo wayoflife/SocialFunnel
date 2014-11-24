8028aa0e44d2edd186507300db610fc500c5eee3
wayoflife <dbetsche@gmail.com>
2014-11-24 15:48:00 +0100

    changed vaadin version to 7.0.7

---
+++ target/.generated/com/google/gwt/lang/softwareengineering_00046SocialFunnel_00046AppWidgetSet__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class softwareengineering_00046SocialFunnel_00046AppWidgetSet__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
