9a1e6c704b1b6cc3e326caa604f1f92e074138ed
wayoflife <dbetsche@gmail.com>
2014-11-24 16:26:06 +0100

    vaadin with spring

--- src/main/java/softwarengineering/SocialFunnel/MyVaadinUI.java
+++ src/main/java/softwarengineering/SocialFunnel/MyVaadinUI.java
@@ -1,8 +1,16 @@
 package softwarengineering.SocialFunnel;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.packtpub.vaadin.User;
+import com.packtpub.vaadin.UserService;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.server.WrappedHttpSession;
+import com.vaadin.server.WrappedSession;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -11,22 +19,30 @@ import com.vaadin.ui.VerticalLayout;
  * The Application's "main" class
  */
 @SuppressWarnings("serial")
-public class MyVaadinUI extends UI
-{
+public class MyVaadinUI extends UI {
 
-    @Override
-    protected void init(VaadinRequest request) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
-        
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
-        });
-        layout.addComponent(button);
-    }
+	private ApplicationContext context;
+
+	@Override
+	protected void init(VaadinRequest request) {
+		UserService service = getUserService(request);
+		User user = service.getUser();
+		String name = user.getName();
+		Label lblUserName = new Label("Hi " + name + " !");
+		VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
+		layout.addComponent(lblUserName);
+	}
+
+	private UserService getUserService(VaadinRequest request) {
+		WrappedSession session = request.getWrappedSession();
+		HttpSession httpSession = ((WrappedHttpSession) session)
+				.getHttpSession();
+		ServletContext servletContext = httpSession.getServletContext();
+		context = WebApplicationContextUtils
+				.getRequiredWebApplicationContext(servletContext);
+		return (UserService) context.getBean("userService");
+	}
 
 }
