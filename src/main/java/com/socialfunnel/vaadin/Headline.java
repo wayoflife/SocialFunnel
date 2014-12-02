28497516cc30f038a3f930db31301a7715e85a42
Simsal <simon_brueckl9@hotmail.com>
2014-12-02 12:20:46 +0100

    headline update

--- src/main/java/com/socialfunnel/vaadin/Headline.java
+++ src/main/java/com/socialfunnel/vaadin/Headline.java
@@ -1,7 +1,10 @@
 package com.socialfunnel.vaadin;
 
 import com.vaadin.ui.Button;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Button.ClickEvent;
 
 public class Headline extends HorizontalLayout {
 	//kmmentar
@@ -11,6 +14,7 @@ public class Headline extends HorizontalLayout {
 	private Button btnEinstellungen = new Button("Einstellungen");
 	private Button btnHilfe = new Button("Hilfe");
 	private Button btnAbmelden = new Button("Abmelden");
+	LogoutListener lgo = new LogoutListener();
 	
 	public Headline() {
 		// TODO Auto-generated constructor stub
@@ -22,7 +26,25 @@ public class Headline extends HorizontalLayout {
 		addComponent(btnHilfe);
 		addComponent(btnAbmelden);
 		
-		//btnAbmelden.addClickListener(LogoutListener);
+		btnStartseite.addClickListener(new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				UI.getCurrent().getNavigator().navigateTo("mainpage");
+			}
+		});
+
+		btnNeuerPost.addClickListener(new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				UI.getCurrent().getNavigator().navigateTo("newpost");
+			}
+		});
+		
+		btnAbmelden.addClickListener(lgo);
 		//btnEinstellungen.addClickListener(EinstellungenListener);
 	
 	}
