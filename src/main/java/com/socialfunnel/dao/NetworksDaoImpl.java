659c942eaeaeda0b671eff84eb7f11bf8026eee2
simon <simon@simon-DH-PC>
2015-04-16 16:31:13 +0200

    Dao implementation

--- src/main/java/com/socialfunnel/dao/NetworksDaoImpl.java
+++ src/main/java/com/socialfunnel/dao/NetworksDaoImpl.java
@@ -1,10 +1,14 @@
 package com.socialfunnel.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Component;
+
+import com.socialfunnel.model.Networks;
 
 @Transactional
 @Component
@@ -12,5 +16,28 @@ public class NetworksDaoImpl implements NetworksDao{
 	
 	@PersistenceContext
 	private EntityManager em;
+	
+	@Override
+	public int save(Networks network) {
+		// TODO Auto-generated method stub
+		em.persist(network);
+		return network.getNetworkId();
+	}
+
+    public void delete(int id){
+    	em.remove(findById(id));
+    }
+
+	@Override
+	public List<Networks> getAll() {
+		// TODO Auto-generated method stub
+        return em.createQuery("SELECT n FROM Networks n", Networks.class).getResultList();
+	}
+
+	@Override
+	public Networks findById(int id) {
+		// TODO Auto-generated method stub
+		return em.createQuery("SELECT n FROM Networks n WHERE networkId ='"+id+"'", Networks.class).getSingleResult();
+	}
 
 }
--- src/main/java/com/socialfunnel/dao/UserDaoImpl.java
+++ src/main/java/com/socialfunnel/dao/UserDaoImpl.java
@@ -1,10 +1,14 @@
 package com.socialfunnel.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Component;
+
+import com.socialfunnel.model.User;
 
 @Transactional
 @Component
@@ -12,6 +16,32 @@ public class UserDaoImpl implements UserDao {
 	
 	@PersistenceContext
 	private EntityManager em;
+
+	@Override
+	public int save(User user) {
+		// TODO Auto-generated method stub
+		em.persist(user);
+		return user.getUserId();
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		em.remove(findById(id));
+		
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+        return em.createQuery("SELECT u FROM User u", User.class).getResultList();
+	}
+
+	@Override
+	public User findById(int id) {
+		// TODO Auto-generated method stub
+		return em.createQuery("SELECT u FROM User u WHERE userId ='"+id+"'", User.class).getSingleResult();
+	}
 
 
 }
