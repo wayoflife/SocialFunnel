de602e5004205bcb18035a12dded3acb21aeef6e
wayoflife <dbetsche@gmail.com>
2015-05-04 12:28:55 +0200

    Tests gesplitted in normale unittests und mocked stuff

---
+++ src/test/java/de/dhbw/socialfunnel/test/MockedTests.java
@@ -0,0 +1,36 @@
+package de.dhbw.socialfunnel.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import de.dhbw.socialfunnel.dao.NetworksDaoImpl;
+import de.dhbw.socialfunnel.dao.UserDaoImpl;
+import de.dhbw.socialfunnel.model.Networks;
+import de.dhbw.socialfunnel.model.User;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockedTests {
+	
+	@Mock private NetworksDaoImpl mockedNetworks;
+	@Mock private UserDaoImpl mockedUsers;
+	
+	@Before
+	public void setUp() {
+		User mockedUser = new User("Otto", "otto@mail.com", "hallo", "11.01.1803");
+		Networks mockedNetwork = new Networks("Facebook", "1", mockedUser);
+		Mockito.when(mockedNetworks.findById(1)).thenReturn(mockedNetwork);
+	}
+	
+	@Test
+	public void getNetworkWithID1() throws Exception {
+		assertEquals("Facebook", mockedNetworks.findById(1).getNetworkName());
+		assertEquals("Otto", mockedNetworks.findById(1).getUser().getName());
+		assertEquals("11.01.1803", mockedNetworks.findById(1).getUser().getDateOfBirth());
+	}
+}
--- src/test/java/de/dhbw/socialfunnel/test/UnitTests.java
+++ src/test/java/de/dhbw/socialfunnel/test/UnitTests.java
@@ -15,19 +15,8 @@ import de.dhbw.socialfunnel.model.Networks;
 import de.dhbw.socialfunnel.model.User;
 import de.dhbw.socialfunnel.todo.PWCrypt;
 
-@RunWith(MockitoJUnitRunner.class)
 public class UnitTests {
-	
-	@Mock private NetworksDaoImpl mockedNetworks;
-	@Mock private UserDaoImpl mockedUsers;
-	
-	@Before
-	public void setUp() {
-		User mockedUser = new User("Otto", "otto@mail.com", "hallo", "11.01.1803");
-		Networks mockedNetwork = new Networks("Facebook", "1", mockedUser);
-		Mockito.when(mockedNetworks.findById(1)).thenReturn(mockedNetwork);
-	}
-	
+
 	@Test
 	public void testEncryption() throws Exception {
 		PWCrypt crypt = PWCrypt.getInstance();
@@ -78,11 +67,4 @@ public class UnitTests {
 	public void dbUser() throws Exception {
 		
 	}	
-	
-	@Test
-	public void getNetworkWithID1() throws Exception {
-		assertEquals("Facebook", mockedNetworks.findById(1).getNetworkName());
-		assertEquals("Otto", mockedNetworks.findById(1).getUser().getName());
-		assertEquals("11.01.1803", mockedNetworks.findById(1).getUser().getDateOfBirth());
-	}
 }
