2d78fb9feec81cedae210013f6cbc58eee9da753
Simsal <simon_brueckl9@hotmail.com>
2015-05-18 14:17:55 +0200

    login form

--- src/main/java/de/dhbw/socialfunnel/security/UserService.java
+++ src/main/java/de/dhbw/socialfunnel/security/UserService.java
@@ -3,54 +3,62 @@ package de.dhbw.socialfunnel.security;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import de.dhbw.socialfunnel.dao.UserDao;
+@Service
 public class UserService implements UserDetailsService {
-	
+
+	@Autowired
+	private UserDao userDao;
+
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
+
 		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		authorities.add(new SimpleGrantedAuthority("CLIENT"));
-		return new User("guest", "password", true, true, false, false, authorities);
-		
-		
+		// return new User("guest", "password", true, true, false, false,
+		// authorities);
+
 		// fetch user from e.g. DB
 		// hier frage an Datenbank nach user
 		// wenn kein user, exception schmeissen
-//		String[] userinfo = new String[] {null,null};
-//		try {
-//			userinfo = new DBHelper().getUserByName(username);
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		if(userinfo[0] == null){
-//			return null;
-//		} else {
-//			authorities.add(new SimpleGrantedAuthority("CLIENT"));
-//			User user = new User(userinfo[0], userinfo[1], true, true, false, false, authorities);
-//			return user;
-//		}
+		String[] userinfo = new String[] { null, null };
+		de.dhbw.socialfunnel.model.User temp = userDao.findByEmail(username);
+		userinfo[0] = temp.getEmail();
+		userinfo[1] = temp.getPassword();
+		// userinfo = new DBHelper().getUserByName(username);
+		if (userinfo[0] == null) {
+			return null;
+		} else {
+			authorities.add(new SimpleGrantedAuthority("CLIENT"));
+			User user = new User(userinfo[0], userinfo[1], true, true, false,
+					false, authorities);
+			return user;
+		}
 		
-//		if ("client".equals(username)) {
-//			authorities.add(new SimpleGrantedAuthority("CLIENT"));
-//			User user = new User(username, "pass", true, true, false, false,
-//					authorities);
-//			return user;
-//		}
-//		if ("admin".equals(username)) {
-//			authorities.add(new SimpleGrantedAuthority("ADMIN"));
-//			User user = new User(username, "pass", true, true, false, false,
-//					authorities);
-//			return user;
-//		} else {
-//			return null;
-//		}
 	}
 }
+
+//if ("client".equals(username)) {
+//	authorities.add(new SimpleGrantedAuthority("CLIENT"));
+//	User user = new User(username, "pass", true, true, false, false,
+//			authorities);
+//	return user;
+//}
+//if ("admin".equals(username)) {
+//	authorities.add(new SimpleGrantedAuthority("ADMIN"));
+//	User user = new User(username, "pass", true, true, false, false,
+//			authorities);
+//	return user;
+//} else {
+//	return null;
+//}
--- src/main/java/de/dhbw/socialfunnel/view/component/LoginFormListener.java
+++ src/main/java/de/dhbw/socialfunnel/view/component/LoginFormListener.java
@@ -1,33 +1,42 @@
 package de.dhbw.socialfunnel.view.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import com.vaadin.navigator.Navigator;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+
+import de.dhbw.socialfunnel.security.AuthManager;
+import de.dhbw.socialfunnel.todo.PWCrypt;
 
 @Component
 public class LoginFormListener implements Button.ClickListener {
 	
-//	@Autowired
-//	private AuthManager authManager;
+	@Autowired
+	private AuthManager authManager;
 
 	@Override
 	public void buttonClick(Button.ClickEvent event) {
-//		try {
-//			Button source = event.getButton();
-//			LoginForm parent = (LoginForm) source.getParent();
-//			String username = parent.getTxtLogin().getValue();
-//			String password =  PWCrypt.getInstance().encrypt(parent.getTxtPassword().getValue());
-//			UsernamePasswordAuthenticationToken request = new UsernamePasswordAuthenticationToken(
-//					username, password);
-//			Authentication result = authManager.authenticate(request);
-//			SecurityContextHolder.getContext().setAuthentication(result);
-//			MyVaadinUI current = (MyVaadinUI) UI.getCurrent();
-//			Navigator navigator = current.getNavigator();
-//			navigator.navigateTo("user");
-//		} catch (AuthenticationException e) {
-//			Notification.show("Authentication failed: " + e.getMessage());
-//		} catch (Exception e) {
-//			Notification.show("Authentication failed: Unknown error");
-//		}
+		try {
+			Button source = event.getButton();
+			LoginForm parent = (LoginForm) source.getParent();
+			String username = parent.getTxtLogin().getValue();
+			String password =  PWCrypt.getInstance().encrypt(parent.getTxtPassword().getValue());
+			UsernamePasswordAuthenticationToken request = new UsernamePasswordAuthenticationToken(
+					username, password);
+			Authentication result = authManager.authenticate(request);
+			SecurityContextHolder.getContext().setAuthentication(result);
+			UI.getCurrent().getNavigator().navigateTo("user");
+		} catch (AuthenticationException e) {
+			Notification.show("Authentication failed: " + e.getMessage());
+		} catch (Exception e) {
+			Notification.show("Authentication failed: Unknown error");
+		}
 	}
 }
