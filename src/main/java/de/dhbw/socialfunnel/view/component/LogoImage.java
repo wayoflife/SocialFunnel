5ed48fb24e804dc97b87d98a99e67da9a9cbaa65
wayoflife <dbetsche@gmail.com>
2015-05-27 14:43:08 +0200

    ich weis nicht wie, aber social config funktioniert :D

--- src/main/java/de/dhbw/socialfunnel/view/component/LogoImage.java
+++ src/main/java/de/dhbw/socialfunnel/view/component/LogoImage.java
@@ -4,8 +4,6 @@ import java.io.File;
 
 import com.vaadin.server.FileResource;
 import com.vaadin.server.VaadinService;
-import com.vaadin.spring.annotation.SpringComponent;
-import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.Image;
 
 public class LogoImage extends Image implements LogoApi{
