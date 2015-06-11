52cf3fbd376b1020c0b204c268be8183c94e691f
Simsal <simon_brueckl9@hotmail.com>
2015-06-11 17:15:11 +0200

    userservice kommentare

--- src/main/java/de/dhbw/socialfunnel/security/UserService.java
+++ src/main/java/de/dhbw/socialfunnel/security/UserService.java
@@ -25,17 +25,11 @@ public class UserService implements UserDetailsService {
 
 		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		authorities.add(new SimpleGrantedAuthority("CLIENT"));
-		// return new User("guest", "password", true, true, false, false,
-		// authorities);
 
-		// fetch user from e.g. DB
-		// hier frage an Datenbank nach user
-		// wenn kein user, exception schmeissen
 		String[] userinfo = new String[] { null, null };
 		de.dhbw.socialfunnel.model.User temp = userDao.findByEmail(username);
 		userinfo[0] = temp.getEmail();
 		userinfo[1] = temp.getPassword();
-		// userinfo = new DBHelper().getUserByName(username);
 		if (userinfo[0] == null) {
 			return null;
 		} 
@@ -52,17 +46,3 @@ public class UserService implements UserDetailsService {
 	}
 }
 
-//if ("client".equals(username)) {
-//	authorities.add(new SimpleGrantedAuthority("CLIENT"));
-//	User user = new User(username, "pass", true, true, false, false,
-//			authorities);
-//	return user;
-//}
-//if ("admin".equals(username)) {
-//	authorities.add(new SimpleGrantedAuthority("ADMIN"));
-//	User user = new User(username, "pass", true, true, false, false,
-//			authorities);
-//	return user;
-//} else {
-//	return null;
-//}
